2f9cdc2b2695680ce7fd57df670e68c76db7d31b
PercTruman <drshawnhart@gmail.com>
2022-04-15 12:18:23 -0500

    cards loading to DOM, working on POST

--- src/index.js
+++ src/index.js
@@ -11,5 +11,59 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
-  });
-});
+   
+  })})
+
+
+  
+  //   let toyObject = {
+  //     name: e.target.name.value,
+  //     image:e.target.image_url.value,
+      
+  //   }
+  // }
+  getAllToys()
+
+  function getAllToys(){
+    fetch('http://localhost:3000/toys')
+      .then(res =>res.json())
+      .then(toyCards =>toyCards.forEach(toyCard=>
+         renderOneToyCard(toyCard)
+      ))}
+
+  function renderOneToyCard(toyCard){
+    let card = document.createElement('li')
+    card.innerHTML = `
+    <div class="card">
+      <h2>${toyCard.name}</h2>
+      <img src = "${toyCard.image}" class="toy-avatar"/>
+      <p>${toyCard.likes}</p>
+      <button class ="like-btn" id=${toyCard.id}>Like ❤️</button>
+    </div>`
+    let cardContainer = document.getElementById('toy-collection')
+    cardContainer.appendChild(card)
+  }
+
+  postNewToy()
+    
+  function postNewToy(newToyObj){
+    let submitButton =document.querySelector('input.submit')
+    
+    submitButton.addEventListener('click', (e)=>
+        e.preventDefault())
+   
+    let inputForm = document.getElementsByTagName('form')
+    console.log(inputForm)
+    fetch('http://localhost:3000/toys',{
+      method: 'POST',
+      headers:{
+          "Content-Type":"application/json",
+          Accept: "application/json"
+          },
+      body: JSON.stringify(newToyObj)
+       })
+      .then(res=>res.json())
+  // }
+  //     // .then(data)
+    
+  }  
